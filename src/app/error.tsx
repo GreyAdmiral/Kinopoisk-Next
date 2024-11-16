6c3492183e0d3bfa5030c5952b255f16fb9b02f9
Binarion <itmaster@duck.com>
2024-11-16 23:29:49 +0300

    feat: Add not found & error components

---
+++ src/app/error.tsx
@@ -0,0 +1,24 @@
+'use client';
+import styles from './error.module.scss';
+
+interface ErrorProps {
+   error: Error | null;
+}
+
+export default function ErrorPage({ error }: ErrorProps) {
+   return (
+      <div className={styles.error}>
+         <p>{error ? `Ошибка: ${error.message}` : 'Неизвестная ошибка!'}</p>
+         <p>{error ? `Причина: ${error.cause}` : 'Причина не установлена!'}</p>
+
+         <button
+            type="button"
+            onClick={() => {
+               window.location.reload();
+            }}
+         >
+            Обновить страницу
+         </button>
+      </div>
+   );
+}
