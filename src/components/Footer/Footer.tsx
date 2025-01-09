e56fa65620573bafec6f35dd61b2a0ff39907a23
Binarion <itmaster@duck.com>
2025-01-09 21:56:42 +0300

    feat: Redesigne footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,14 +2,30 @@ import { Container } from '@components/Container/Container';
 import styles from './Footer.module.scss';
 
 export const Footer = () => {
+   const developerlink = 'https://github.com/GreyAdmiral';
+   const shoolLink = 'https://rs.school/';
+   const devLinkTitleAttribut = 'Открыть репозиторий разработчика';
+   const shoolLinkTitleAttribut = 'Открыть сайт школы';
+   const developerNickName = 'B!n@r!0n';
+   const shoolName = 'RSScool';
+
    return (
       <footer className={styles.footer}>
          <Container className={styles.footer}>
             <div className={styles.footer_body}>
-               Проект создан в рамках программы обучения школы{' '}
-               <a href="https://rs.school/" title="Открыть сайт школы" target="_blank">
-                  RSScool
-               </a>
+               <span className={styles.footer_shool}>
+                  Благодарность школе{' '}
+                  <a href={shoolLink} title={shoolLinkTitleAttribut} target="_blank">
+                     {shoolName}
+                  </a>
+               </span>
+
+               <span className={styles.footer_developer}>
+                  &copy;{' '}
+                  <a href={developerlink} title={devLinkTitleAttribut}>
+                     {developerNickName}
+                  </a>
+               </span>
             </div>
          </Container>
       </footer>
