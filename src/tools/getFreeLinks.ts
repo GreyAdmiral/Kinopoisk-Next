9b4ca076b50f5572634964edcbdca1bb7b75bb3a
Binarion <itmaster@duck.com>
2025-06-14 19:35:15 +0300

    fix: Fixed mirror link; feat: Added mirror links;

--- src/tools/getFreeLinks.ts
+++ src/tools/getFreeLinks.ts
@@ -1,9 +1,13 @@
 export function getFreeLinks(id: string) {
-   const freeLink = `https://flicksbar.mom/film/${id}/`;
+   const freeLink = `https://flcksbr.top/film/${id}/`;
+   const freeLinkTwo = `https://n1.kinosave.buzz/film/${id}/`;
    const mirrorLink = `https://vavada-pro.com/#${id}`;
+   const mirrorLinkTwo = `https://www.kinopoisk.cx/film/${id}/`;
 
    return {
       freeLink,
+      freeLinkTwo,
       mirrorLink,
+      mirrorLinkTwo,
    };
 }
