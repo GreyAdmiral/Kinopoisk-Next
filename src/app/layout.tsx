1f8f4255d739e34d0ceca48c3215bde1327f4cdd
Binarion <itmaster@duck.com>
2025-08-28 05:08:21 +0300

    fix: Head in  layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,14 @@ import { Container } from '@components/Container/Container';
 import { Header } from '@components/Header/Header';
 import { Footer } from '@components/Footer/Footer';
 import type { PropsWithChildren } from 'react';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import styles from './layout.module.scss';
 import '../scss/style.scss';
+
+export const viewport: Viewport = {
+   initialScale: 1,
+   width: 'device-width',
+};
 
 export const metadata: Metadata = {
    title: 'Неофициальный кинопоиск',
@@ -15,9 +20,13 @@ export default function RootLayout({ children }: PropsWithChildren) {
    return (
       <html lang="ru">
          <head>
+            <meta name="format-detection" content="telephone=no, email=no, address=no, date=no" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta httpEquiv="msthemecompatible" content="no" />
+            <meta httpEquiv="imagetoolbar" content="no" />
             <link rel="icon" href="./favicon.ico" sizes="any" />
-            <link rel="icon" href="./icon?<generated>" type="image/<generated>" sizes="<generated>" />
-            <link rel="apple-touch-icon" href="./apple-icon?<generated>" type="image/<generated>" sizes="<generated>" />
+            <link rel="icon" href="./icon.png" type="image/png" sizes="32x32" />
+            <link rel="apple-touch-icon" href="./apple-icon.png" type="image/png" sizes="32x32" />
          </head>
 
          <body>
