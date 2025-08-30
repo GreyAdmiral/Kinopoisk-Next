72e52277042f5e99330d0f11259b66779080687a
Binarion <itmaster@duck.com>
2025-08-30 03:52:19 +0300

    fix: Fixed Header title text

--- src/components/HeaderLink/HeaderLink.tsx
+++ src/components/HeaderLink/HeaderLink.tsx
@@ -1,15 +1,14 @@
 'use client';
 import Link from 'next/link';
-import { AppRoutes } from '@tools/costants';
+import { AppRoutes, brandTitle } from '@tools/costants';
 import styles from './HeaderLink.module.scss';
 
 export const HeaderLink = () => {
-   const headerTitle = 'Неофициальный кинопоиск';
    const headerTitleAttribute = 'Перейти на главную страницу';
 
    return (
       <Link href={AppRoutes.HOME_ROUTE} className={styles.header_title_link} title={headerTitleAttribute}>
-         {headerTitle}
+         {brandTitle}
       </Link>
    );
 };
