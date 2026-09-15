0b1d9853f42b2ddc508d12b7edf7820dd63dbf04
binarion <itmaster@duck.com>
2026-09-15 23:05:41 +0300

    feat: Updated settings & browserlist

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const nextConfig = {
    reactStrictMode: true,
+   poweredByHeader: false,
+   productionBrowserSourceMaps: false,
+   cleanDistDir: true,
+   output: 'standalone',
    images: {
       formats: ['image/avif', 'image/webp'],
       remotePatterns: [
