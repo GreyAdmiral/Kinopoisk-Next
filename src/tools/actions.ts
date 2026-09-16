6db08e93055eb83abef341930bba1e7a6b6044db
binarion <itmaster@duck.com>
2026-09-16 07:10:54 +0300

    fix: Fixed search action

--- src/tools/actions.ts
+++ src/tools/actions.ts
@@ -7,7 +7,7 @@ export async function searchAction(data: FormData) {
    const searchParams = new URLSearchParams(queries);
 
    if (keyword) {
-      searchParams.set('keyword', encodeURIComponent(keyword));
+      searchParams.set('keyword', keyword);
    } else if (searchParams.has('keyword')) {
       searchParams.delete('keyword');
    }
